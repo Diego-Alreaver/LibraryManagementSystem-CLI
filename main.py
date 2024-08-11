ee9d0a7f3c0d3b76a62bc09a55867788c27565f4
Diego-Alreaver <diego8558@hotmail.com>
2024-08-11 14:37:17 -0400

    Agregado menu de entrada

--- main.py
+++ main.py
@@ -2,10 +2,28 @@ import sys
 from libreria import Libreria
 
 def main():
+    opc = ''
     libreria = Libreria("Libreria Al")
     libreria.agregarLibroCSV()
-    libreria.agregarLibroManual()
-    libreria.informacion()
+    while opc != '5':
+        print(f"Bienvenido a la librería {libreria.nombre}, estas son sus opciones:")
+        print("1) Agregar un libro")
+        print("2) Revisar último libro agregado")
+        print("5) Salir")
+        
+        opc = input("Seleccione una opción: ")
 
+        # Validar que la opción sea un número y esté dentro de las opciones disponibles
+        if opc not in ['1', '2', '5']:
+            print("Opción no válida. Por favor, seleccione una opción válida.")
+            continue
+
+        if opc == '1':
+            libreria.agregarLibroManual()
+        elif opc == '2':
+            libreria.informacion()
+        elif opc == '5':
+            print("Saliendo del programa...")
+            break
 
 main()
