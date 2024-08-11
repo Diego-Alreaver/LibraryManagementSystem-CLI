649cb4b6a6d8c89c00da4e16205302aa26e20b9d
Diego-Alreaver <diego8558@hotmail.com>
2024-08-11 13:27:49 -0400

    Se pueden agregar libros manualmente

--- libreria.py
+++ libreria.py
@@ -9,8 +9,6 @@ class Libreria:
         self.capacidad_maxima = capacidad_maxima #capacidad maxima de la libreria, probablemente sin uso
         self.usuarios = [] #lista de nombres de usuarios registrados
  
-
-
     def agregarLibroCSV(self):
         with open("libros.csv", "r") as archivo_csv:
             lector = csv.reader(archivo_csv)
@@ -24,5 +22,23 @@ class Libreria:
                 nuevo_libro = books(titulo, autor, genero, año, isbn, disponibilidad)
                 self.libros.append(nuevo_libro)
 
+    def agregarLibroManual(self):
+        titulo = input("Ingrese el título del libro: ")
+        autor = input("Ingrese el autor del libro: ")
+        genero = input("Ingrese el género del libro: ")
+        anio_publicacion = input("Ingrese el año de publicación del libro: ")
+        isbn = input("Ingrese el ISBN del libro: ")
+        estado = "Disponible"
+        
+        newbook = books(titulo, autor, genero, anio_publicacion, isbn, estado)
+        self.libros.append(newbook)
+
+        # Escribir los datos a un archivo CSV
+        with open("libros.csv", "a", newline='') as archivo_csv:
+            escritor = csv.writer(archivo_csv)
+            # Convertir el objeto en una lista de sus atributos
+            escritor.writerow([newbook._title, newbook._author, newbook._genre, 
+                               newbook._publish_year, newbook._ISBN, newbook._disponible])
+            
     def informacion(self):
-        print(self.libros[0])
+        print(self.libros[len(self.libros)-1])
